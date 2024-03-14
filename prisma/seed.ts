8328a589757ceb8d03aa47ba472284a866f0f7b9
mohammed sameer <85784627+mohammed3895@users.noreply.github.com>
2024-03-14 22:02:30 +0200

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,6 +8,7 @@ async function main() {
       create: {
         name: "Guillermo Rauch",
         email: "rauchg@vercel.com",
+        password: "pass582587",
         image:
           "https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg",
       },
@@ -18,6 +19,7 @@ async function main() {
       create: {
         name: "Lee Robinson",
         email: "lee@vercel.com",
+        password: "pass78946",
         image:
           "https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg",
       },
@@ -28,6 +30,7 @@ async function main() {
       create: {
         name: "Steven Tey",
         email: "stey@vercel.com",
+        password: "123456789",
         image:
           "https://images.ctfassets.net/e5382hct74si/4QEuVLNyZUg5X6X4cW4pVH/eb7cd219e21b29ae976277871cd5ca4b/profile.jpg",
       },
